e3e3ae64b2a76856fb5859955d4ee5b2278213a8
JonnyDEWENT <32647918+JonnyDEWENT@users.noreply.github.com>
2020-04-18 13:57:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -61,30 +61,54 @@ function setFireBaseRefs(){
 
   //Reference to the subtitle portion of the db
   subTitleRef = screenRef.child('subtitle');  
+
+  vue.tabs.pop()
 }
 
 function setVueFirebaseConnections(){
   
   //Tab data listener
   tabsRef.on('value', function(snapshot) {
-    for (i = 0; i < snapshot.val().length; i++){
-    var anotherChild = snapshot.val()[i].title
-    var cardsChild = snapshot.val()[i].cards
+
+    vue.tabs = []
+
+    var dictionary = snapshot.val()
+
+    for (var key in dictionary) {
+      // check if the property/key is defined in the object itself, not in parent
+      if (dictionary.hasOwnProperty(key)) {           
+          console.log(key, dictionary[key]);
+
+          vue.tabs.push({title: key, index:dictionary[key].index, cards: dictionary[key].cards})
+      }
+  }
+
+  vue.FirebaseOrderofTabs = vue.tabs
+  sortOnKeys(vue.tabs)
+
+
+
+  //   console.log(snapshot.val().length)
+  //   for (i = 0; i < snapshot.val().length; i++){
+  //     try{
+      
+  //   var anotherChild = snapshot.val()[i].title
+  //   var cardsChild = snapshot.val()[i].cards
     
     
-    if (vue.tabs.length > i){
-    vue.tabs[i].title = anotherChild
-    vue.tabs[i].cards = cardsChild
+  //   if (vue.tabs.length > i){
+  //   vue.tabs[i].title = anotherChild
+  //   vue.tabs[i].cards = cardsChild
 
 
-    }
-    else {
-    vue.tabs.push({title: anotherChild, cards: cardsChild})
-    }
+  //   }
+  //   else {
+  //   vue.tabs.push({title: anotherChild, cards: cardsChild})
+  //   }
+  // }
+  // catch(error){}
 
-
-
-    }
+  //   }
   
   });
   
@@ -101,6 +125,21 @@ function setVueFirebaseConnections(){
   });
 }
 
+//Sort tabs
+function sortOnKeys(dict) {
+  var tempdictionary = []
+
+  for (var key in dict) {
+    // check if the property/key is defined in the object itself, not in parent
+    if (dict.hasOwnProperty(key)) {           
+        console.log(key, dict[key]);
+        tempdictionary[dict[key].index] = {title: dict[key].title, index: dict[key].index, cards: dict[key].cards}
+    }
+}
+
+vue.tabs = tempdictionary
+}
+
 
 //Initializing vue instance for this web app
 function initialize() {
@@ -111,6 +150,8 @@ function initialize() {
 
       //tabs - need to rename this later
       tabs: [{title: "", cards:[]}],
+
+      FirebaseOrderofTabs: [{title: "", cards:[]}],
 
       //what screen are we on (2 is the main, 1 is login, can add more)
       screen: 2,
@@ -152,15 +193,16 @@ function initialize() {
         for (i = 0; i < vue.tabs.length; i++){
           console.log(vue.tabs[i].title)}
         
-        tabsRef.set(vue.tabs)
-        titleRef.set(vue.appTitle)
-        subTitleRef.set(vue.subTitle)
+          tabsRef.child(vue.tabs.length).set({title: vue.newItem, cards: []})
+          titleRef.set(vue.appTitle)
+          subTitleRef.set(vue.subTitle)
       },
 
       //Adds a new item to the tabs in our view instance and also pushes this value to firebase
       addToNavBar(name){
-        vue.tabs.push({title: name})
-        this.updateNavBar()
+       // vue.tabs.push({title: name, cards: []})
+        //this.updateNavBar()
+        tabsRef.child(name).set({title: name, index: vue.tabs.length, cards: []})
       },
       //#endregion
 
@@ -173,6 +215,10 @@ function initialize() {
         tabsRef.child(vue.option).child("cards").set(vue.tabs[vue.option].cards) 
         console.log(vue.tabs)
         console.log(vue.newImage)
+      },
+      
+      deleteFromNavBar(title){
+        tabsRef.child(title.title).remove()
       },
 
       getCards(){
